e256e0fd051484efd3667bc3e9d7073a9049767a
student <student>
2019-05-07 19:08:23 +0300

    datastructures in python

---
+++ quiz.py
@@ -0,0 +1,90 @@
+def tens(numbers):
+	d=[]
+	for number in numbers:
+		numbers=[1,2,3,4,5,6,7,8,9]
+		r=number*10
+		d.append(r)
+		return(a)
+
+		
+
+def squares(squares):
+	even=[2,4,6,8,10,12]
+	for x in squares:
+		squares.append(x)
+		return(squares)
+
+
+def sorted(a,b):
+	a=[9,1,4,7,3]
+	b=[5,2,6,8,0]
+	z="a"+"b"
+	sort.sorted(z)
+
+
+def range_sum(number,n):
+	for x in range(1,n):
+		w=(sum 1,n):
+		print(w)
+
+
+
+def largest(integers):
+	largest=([1,3,6,8,2,4,5.7]):
+	a.largest([1,3,6,8,2,4,5.7]):
+		print([3])
+
+
+def smallest(integers):
+	smallest=([1,3,6,8,2,4,5.7]):
+	a.smallest([1,3,6,8,2,4,5.7]):
+		print([0])
+
+
+
+def my_modulus(range):
+	for x in range(10,20):
+		if x%3==0:
+			print(x)
+
+
+
+
+def student(name,balance,):
+	student1[{"balance":1000,"name":"Irene",}]
+	student2[{"balance":2000,"name":"Puline"}]
+	student3[{"balance":3000,"name":"Naima"}]
+	student4[{"balance":1000,"name":"Rose"}]
+		for student in students
+		sms("hello {},your {} is {}".format(student,"name","balance"))
+			print(sms)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+		
